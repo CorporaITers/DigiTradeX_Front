e55c7ebf15942d961252d87ff54edb6de07762a0
Yoshio Nomura <nomurin.alex103@gmail.com>
2025-04-11 12:19:19 +0900

    8th commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'public'),
   plugins: [react()],
 });
